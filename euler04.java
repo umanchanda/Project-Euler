8a969bca17161be50e6a549336dc62222d6b7d5e
Uday Manchanda <uday.manchanda14@gmail.com>
2021-11-24 01:29:34 -0500

    fix indentation

--- euler04.java
+++ euler04.java
@@ -1,41 +1,41 @@
 import java.io.*;
 import java.util.*;
 public class euler4 {
-	public static void main(String[] args) {
-		ArrayList<Integer> mylist = new ArrayList<Integer>();
-		int product = 0;
-		for (int a = 100; a <= 999; a++) {
-			for (int b = 100; b <= 999; b++) {
-				product = a * b;
-				String prod = Integer.toString(product);
-				if (isPalindrome(prod)) {
-					int c = Integer.parseInt(prod);
-					mylist.add(c);
-				}
-			}
-		}
-		Collections.sort(mylist);
-		System.out.println(mylist);
-	}
-	
-	public static boolean isPalindrome(String s) {
-		StringBuilder sb = new StringBuilder(s);
-		StringBuilder revsb = new StringBuilder(sb.reverse());
-		String rev = revsb.toString();
-		
-		if (s.equals(rev)) {
-			return true;
-		}
-		return false;
-	}
-	
-	/* public static int findMax(ArrayList<Integer> list) {
-		int max = 0;
-		for (int i : list) {
-			if (max > i) {
-				max = i;
-			}
-		}
-		return max;
-	} */
+  public static void main(String[] args) {
+    ArrayList<Integer> mylist = new ArrayList<Integer>();
+    int product = 0;
+    for (int a = 100; a <= 999; a++) {
+      for (int b = 100; b <= 999; b++) {
+        product = a * b;
+        String prod = Integer.toString(product);
+        if (isPalindrome(prod)) {
+          int c = Integer.parseInt(prod);
+          mylist.add(c);
+        }
+      }
+    }
+    Collections.sort(mylist);
+    System.out.println(mylist);
+  }
+  
+  public static boolean isPalindrome(String s) {
+    StringBuilder sb = new StringBuilder(s);
+    StringBuilder revsb = new StringBuilder(sb.reverse());
+    String rev = revsb.toString();
+    
+    if (s.equals(rev)) {
+      return true;
+    }
+    return false;
+  }
+  
+  /* public static int findMax(ArrayList<Integer> list) {
+    int max = 0;
+    for (int i : list) {
+      if (max > i) {
+        max = i;
+      }
+    }
+    return max;
+  } */
 }
--- euler06.java
+++ euler06.java
@@ -1,16 +1,16 @@
 public class euler6 {
-	public static void main(String[] args) {
-		long squaresum = sum(100) * sum(100);
-		long squaretotal = sum1(100) / 6;
-		long difference = squaresum - squaretotal;
-		System.out.println(difference);
-	}
-	
-	public static long sum(long n) {
-		return (n * (n+1) / 2);
-	}
-	
-	public static long sum1(long n) {
-		return (n * (n+1) * (2*n+1));
-	}
+  public static void main(String[] args) {
+    long squaresum = sum(100) * sum(100);
+    long squaretotal = sum1(100) / 6;
+    long difference = squaresum - squaretotal;
+    System.out.println(difference);
+  }
+  
+  public static long sum(long n) {
+    return (n * (n+1) / 2);
+  }
+  
+  public static long sum1(long n) {
+    return (n * (n+1) * (2*n+1));
+  }
 }
