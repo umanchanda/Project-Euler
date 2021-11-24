b49cd3b868fcd722c837acd44a023ec3cd7f85d5
Uday Manchanda <uday.manchanda14@gmail.com>
2021-11-23 22:09:08 -0500

    fix identation

--- euler12.py
+++ euler12.py
@@ -1,20 +1,20 @@
 def triangle(n):
-    return (n * n-1) // 2
+  return (n * n-1) // 2
 
 def numdivisors(n):
-    a = 0
-    for i in range(1, n+1):
-        if n % i  == 0:
-            a += 1
-    return a
+  a = 0
+  for i in range(1, n+1):
+    if n % i  == 0:
+      a += 1
+  return a
 
 triangle_numbers = []
 for i in range(1, 1000):
-    triangle_numbers.append(triangle(i))
+  triangle_numbers.append(triangle(i))
 
 triangle_divisors = {}
 for j in triangle_numbers:
-    triangle_divisors[j] = numdivisors(j)
+  triangle_divisors[j] = numdivisors(j)
 
 for k,v in triangle_divisors:
-    print(k, v)
+  print(k, v)
