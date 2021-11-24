516b152ec388116036e7056a480ff32ca0dcddc3
Uday Manchanda <uday.manchanda14@gmail.com>
2021-11-24 01:32:52 -0500

    fix indentation

--- euler50.py
+++ euler50.py
@@ -1,15 +1,15 @@
 def is_prime(num):
-	for i in range(2, num-1):
-		if num % i == 0:
-			return False
-	return True
+  for i in range(2, num-1):
+    if num % i == 0:
+      return False
+  return True
 
 lst_of_primes = []
 num = 2
 while num < 1000:
-	if is_prime(num):
-		lst_of_primes.append(num)
-	num = num + 1
+  if is_prime(num):
+    lst_of_primes.append(num)
+  num = num + 1
 
 
 print(lst_of_primes)
